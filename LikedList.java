c15fa0d182bdf369c70946c78c43c0ca884e1f62
JoseCustodio23 <88993981+JoseCustodio23@users.noreply.github.com>
2022-05-18 14:48:33 -0400

    Create LikedList.java
    Programa para invertir la lista

---
+++ LikedList.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Reverse {
+    public static void main(String[] argv)
+        throws Exception 
+    {
+    
+        try {
+    // Create the LinkedList
+    LinkedList<String> price = new LinkedList<String>();
+    
+    // Add elemnts into the LinkedList with add() method
+    price.add("1500");
+    price.add("1350");
+    price.add("950");
+    price.add("800");
+    price.add("450");
+    price.add("300");
+    price.add("150");
+    
+    // Show all the elements in normal order
+    System.out.println("LinkedList:" + price);
+    
+    Iterator x = price.descendingIterator();
+    
+    // Show all the elements in descending order
+    while (x.hasNext()) {
+        System.out.println("Value is : "
+                            + x.next());
+        }
+    }
+    
+    catch (NullPointerException e) {
+        System.out.println("Exception thrown : "
+                            + e);
+            }
+        }    
+    }
